daf58886596b2f3beb4bab46dd456c579897b738
Reis Can Kayaer <reiscankayaer@gmail.com>
2026-04-19 13:38:01 +0300

    JS ilk ödev tamamlandı,düzenlendi

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,9 @@
-function getElementWidth (content, padding, border) { 
-    const contentValue = Number.parseFloat(content);
-    const paddingValue = Number.parseFloat(padding);
-    const borderValue = Number.parseFloat(border);
-    const totalWidth = contentValue + (paddingValue * 2) + (borderValue * 2);
-    console.log(`${totalWidth}`);
-    return totalWidth;
+function getElementWidth(content, padding, border) {
+  const contentValue = Number.parseFloat(content);
+  const paddingValue = Number.parseFloat(padding);
+  const borderValue = Number.parseFloat(border);
+  const totalWidth = contentValue + paddingValue * 2 + borderValue * 2;
+  return `${totalWidth}`;
 }
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
